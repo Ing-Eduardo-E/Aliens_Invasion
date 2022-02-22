8f07185073fb0740efa776d8c2997a6a2c800e02
Eduardo E. Enríquez R <rekineke@hotmail.com>
2022-02-21 21:48:23 -0500

    Movimientos izquierda y derecha de la nave

--- ship.py
+++ ship.py
@@ -15,6 +15,17 @@ class Ship:
         # coloca inicialmente cada nave nueva en el centro de la parte inferior de la pantalla
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Bandera de movimiento
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Actualiza la posición de la nave en funcion de la bandera de movimiento"""
+        if self.moving_right:
+            self.rect.x +=1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """dibuja la nave en su ubicación actual"""
         self.screen.blit(self.image, self.rect)
